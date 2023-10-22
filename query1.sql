958e47ef0517568eaf8ad8bef6c0b8813cebb507
Eric Lai <jlai38@ucsc.edu>
2023-10-22 15:47:22 -0700

    query 1 uploaded

---
+++ query1.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT e.editionDate AS theEditionDate
+FROM Editions e
+WHERE EXISTS (SELECT *
+            FROM Articles a
+            WHERE e.editionDate = a.editionDate
+            And a.articleNum > e.numArticles
+            AND a.articlePage > e.numPages );
+
+
+
+
